068d2173bf9b72a1e3f6492b823e3292b4bd6f12
Adesh Singh <adeshsingh3002@gmail.com>
2025-04-05 14:25:12 +0530

    Initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Homepage() {
   return (
     <div>
       Home Page
+      <Link to="/dashboard/overview">Dashboard</Link>
     </div>
   )
 }
